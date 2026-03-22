9fb9f08954c6218d29a311de143e0dd0663f3e26
brrrief <welco.omehom3@gmail.com>
2026-03-22 10:47:37 +0300

    Create Tooltip.js

---
+++ Tooltip.js
@@ -0,0 +1,37 @@
+// Загружаем Popper и Tippy
+function loadTippy(callback) {
+  const popper = document.createElement('script');
+  popper.src = 'https://unpkg.com/@popperjs/core@2';
+  popper.onload = function() {
+
+    const tippyScript = document.createElement('script');
+    tippyScript.src = 'https://unpkg.com/tippy.js@6';
+    tippyScript.onload = callback;
+
+    document.head.appendChild(tippyScript);
+  };
+
+  document.head.appendChild(popper);
+}
+
+// Инициализация тултипов
+function initializeTooltips() {
+  const elements = document.querySelectorAll('[ms-code-tooltip-right]');
+
+  elements.forEach(element => {
+    tippy(element, {
+      content: `<div class="tooltip-text">${element.getAttribute('ms-code-tooltip-right')}</div>`,
+      placement: 'right',
+      followCursor: element.hasAttribute('ms-code-tooltip-followCursor')
+        ? element.getAttribute('ms-code-tooltip-followCursor')
+        : false,
+      theme: 'white-custom',
+      arrow: false,
+      allowHTML: true
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadTippy(initializeTooltips);
+});
